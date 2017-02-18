8b20a0f98cd6233d83f4a164627301ba19f02382
tzaeru <tzaeru@gmail.com>
2017-02-19 00:22:51 +0200

    BA chat channel

--- store/SettingsOverride.js
+++ store/SettingsOverride.js
@@ -1,5 +1,12 @@
 // This module is to be overriden by custom lobbies deriving from swl.
-module.exports.settings = {"Spring games to show in battle list": {
+module.exports.settings = {
+
+			"Chat": {
+				autoJoin: { val: "#ba", name: 'Autojoin channels/private messages', type: 'list', desc: 'Can be changed by pressing the heart button in chat. Channels are prefixed with #.' },
+				sortColors: { val: false, name: 'Sort players by rank', type: 'bool' },
+			},
+
+			"Spring games to show in battle list": {
 				selectedAll: { val: false, name: 'Always show all games', type: 'bool' },
 				selectedBa: { val: true, name: 'Balanced Annihilation', type: 'bool' },
 			},};
